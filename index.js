948aebc7f3a259f0fa96264eead4505261f3db56
Amio <amio.cn@gmail.com>
2013-01-13 23:14:29 +0800

    Remove focus-cross switch

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 'use strict';
 
 (function(){
-  var crossSizeLoop = {
-//    'focus-cross-10': 'focus-cross-20',
-    'focus-cross-20': 'focus-cross-30',
-    'focus-cross-30': 'focus-cross-40',
-    'focus-cross-40': 'focus-cross-20'
-  };
-
-  var $cross = $('#focus-cross');
-  $(document.body).on('click',function(){
-    $cross.attr('class',crossSizeLoop[$cross.attr('class')])
-  });
+  //
 })();
