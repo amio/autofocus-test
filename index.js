0263b6e8c0b2120d93df6aeecb752f14aa42cd46
Amio <amio.cn@gmail.com>
2013-01-13 18:53:22 +0800

    Use css background to generate lines instead of using dom

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 'use strict';
 
 (function(){
-  var markerTmpl = $('#marker-tmpl').text();
-
-  var makeLines = function(num){
-    var htmlRaw = '';
-    var lineTmpl = $('#line-tmpl').text();
-    for(;num--;){
-      htmlRaw += lineTmpl;
-    }
-    return htmlRaw;
+  var crossSizeLoop = {
+//    'focus-cross-10': 'focus-cross-20',
+    'focus-cross-20': 'focus-cross-30',
+    'focus-cross-30': 'focus-cross-40',
+    'focus-cross-40': 'focus-cross-20'
   };
 
-  var makeTestCanvas = function(lineHeight){
-    var winHeight = document.body.scrollHeight;
-    var halfLines = makeLines(Math.round(winHeight/lineHeight/2));
-
-    return halfLines + markerTmpl + halfLines;
-  };
-
-  $('#wrapper').html(makeTestCanvas(10));
-
-  window.app = {
-    makeLines: makeLines,
-    makeTestCanvas: makeTestCanvas
-  }
+  var $cross = $('#focus-cross');
+  $(document.body).on('click',function(){
+    $cross.attr('class',crossSizeLoop[$cross.attr('class')])
+  });
 })();
